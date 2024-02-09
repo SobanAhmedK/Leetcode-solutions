cb375eaf210f1e675c56298f271fea387adfee4e
Soban <ahmedsoban648@gmail.com>
2024-02-10 01:21:49 +0500

    medium level

--- ArrayProductAcceptSelf.py
+++ ArrayProductAcceptSelf.py
@@ -14,3 +14,18 @@
 # The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 # Follow up: Can you solve the problem in O(1) extra space complexity?
 # (The output array does not count as extra space for space complexity analysis.)
+
+# class Solution(object):
+#     def productExceptSelf(self, nums):
+
+class Solution:
+  def productExceptSelf(self, nums):
+    res = [1] * (len(nums))
+
+    for i in range(1, len(nums)):
+        res[i] = res[i-1] * nums[i-1]
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= postfix
+        postfix *= nums[i]
+    return res
