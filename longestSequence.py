922bd1f462498453fefc38ce7affeb2371779682
Soban <ahmedsoban648@gmail.com>
2024-02-12 23:47:28 +0500

    medium problem

--- longestSequence.py
+++ longestSequence.py
@@ -10,3 +10,30 @@
 # Output: 9
 # #
 
+
+class Solution(object): 
+   
+
+   def longestConsecutive( self,nums):
+        if not nums:
+            return 0
+
+        set_nums = set(nums)
+        n = sorted(set_nums, key=lambda x: (x >= 0, x))
+        counting = n[0]
+        l = 1
+        maxlength = 1  
+
+        for num in n[1:]: 
+            if num == counting + 1:
+                counting = num
+                l += 1
+                maxlength = max(maxlength, l)  
+            else:
+                counting = num
+                l = 1 
+
+        return maxlength
+    
+    
+    #in this solution on larger inputs complexity will be O(N logN) because or sorting function
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+nums = [9, 1, -3, 2, 4, 8, 3, -1, 6, -2, -4, 7]
+sorted_nums = sorted(nums, key=lambda x: (x < 0, -x))
+print(sorted_nums)
